dc390965c3bbf78926b3769ff73381abb7ab0794
Bárbara Tanizawa <barbaratanizawa@gmail.com>
2025-08-05 18:27:46 -0300

    Update exercicio_2.py

--- exercicio_2.py
+++ exercicio_2.py
@@ -3,7 +3,7 @@
 
 Receba uma lista de input do usuário.
 
-Ele digitará como um texto com os números separados por vígula. Para isso, pode-se utilizar o código disponibilizado que
+Ele digitará como um texto com os números separados por vírgula. Para isso, pode-se utilizar o código disponibilizado que
 vai transformar esse texto em lista para você.
 
 Depois imprima uma lista apenas com os números ímpares.
@@ -22,4 +22,11 @@ Os números ímpares são [1, 3, 5]
 # Código para pegar a lista
 lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))]
 
-# Fazer a partir daqui...
+# Criar lista apenas com ímpares
+impares = []
+
+for numero in lista:
+    if numero % 2 != 0:
+        impares.append(numero)
+
+print(f"Os números ímpares são {impares}")
